25f4366fa480a35f0fb17223d4cbf7042d1e7d80
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2017-02-21 02:40:43 -0500

    Included simple script.

---
+++ scripts/tweet.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# coding=utf-8
+
+import sys
+from os import path
+
+if len(sys.argv) != 2:
+	print '''
+	The script requires one input argument
+
+	Example
+	-------
+	python tweet.py "Look at me, posting from terminal!"
+	'''
+
+tweet_text = sys.argv[1]
+
+if not path.exists( 'login.py' ):
+	print('Login script does not exist. Exiting script.')
+	quit()
+else:
+	execfile( 'login.py' )
+
+r = api.request('statuses/update', {'status': tweet_text})
