9ee417029664baa4992e8212d930ca6631e11530
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2017-02-21 02:20:25 -0500

    Cleaned up example.

--- examples/post_from_terminal.py
+++ examples/post_from_terminal.py
@@ -1,3 +1,7 @@
-execfile( 'login.py' )
+if not path.exists( 'login.py' ):
+	print('Login script does not exist. Exiting script.')
+	quit()
+else:
+	execfile( 'login.py' )
 
 r = api.request('statuses/update', {'status':'Tweeting from terminal...'})
