58e84f16dc9f01efe05448458eb133c62d5e8741
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2017-06-06 22:33:22 -0400

    Updated scripts.

--- accounts/BarrioWRCT883/movies_riverview_park.sh
+++ accounts/BarrioWRCT883/movies_riverview_park.sh
@@ -2,7 +2,7 @@
 
 echo "Tweeting about movies in the park in Riverview Park"
 IMAGE="../../images/movies_riverview_park.png"
-MESSAGE="Recuerden que ésta noche en Riverview Park, @PghEventsOffice mostrará una película a la caída del sol http://bit.ly/1kRS6Gx"
+MESSAGE="Recuerden que ésta noche en Riverview Park, @PghEventsOffice mostrará una película a la caída del sol http://bit.ly/2aZpav7"
 
 if [ -f $IMAGE ]; then
 	python tweet_and_upload_a_picture.py "$MESSAGE" "$IMAGE"
--- accounts/BarrioWRCT883/movies_schenley_plaza.sh
+++ accounts/BarrioWRCT883/movies_schenley_plaza.sh
@@ -2,9 +2,9 @@
 
 echo "Tweeting about movies in the park in Schenley Plaza"
 IMAGE="../../images/movies_schenley_plaza.png"
-MESSAGE="Recuerden que ésta noche en Schenley Plaza, @PghEventsOffice mostrará una película a la caída del sol http://bit.ly/1HcGGXZ"
+MESSAGE="Recuerden que ésta noche en Schenley Plaza, @PghEventsOffice mostrará una película a la caída del sol http://bit.ly/2s28XiE"
 
-if [ ! -f $IMAGE ]; then
+if [ -f $IMAGE ]; then
 	python tweet_and_upload_a_picture.py "$MESSAGE" "$IMAGE"
 else
 	python tweet.py "$MESSAGE"
